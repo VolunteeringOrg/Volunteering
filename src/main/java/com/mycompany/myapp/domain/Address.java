8999ea5280aac44b1982a7028cfb6ab9e283a84c
lxuserb <zalewmic@gmail.com>
2017-05-30 20:18:18 +0200

    Entity: Address

---
+++ src/main/java/com/mycompany/myapp/domain/Address.java
@@ -0,0 +1,153 @@
+package com.mycompany.myapp.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Address.
+ */
+@Entity
+@Table(name = "address")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@Document(indexName = "address")
+public class Address implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "email", length = 255, nullable = false)
+    private String email;
+
+    @Size(max = 255)
+    @Column(name = "street", length = 255)
+    private String street;
+
+    @Size(max = 255)
+    @Column(name = "city", length = 255)
+    private String city;
+
+    @Size(max = 255)
+    @Column(name = "postal_code", length = 255)
+    private String postalCode;
+
+    @Size(max = 255)
+    @Column(name = "country", length = 255)
+    private String country;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Address email(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public Address street(String street) {
+        this.street = street;
+        return this;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Address city(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public Address postalCode(String postalCode) {
+        this.postalCode = postalCode;
+        return this;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Address country(String country) {
+        this.country = country;
+        return this;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        if (address.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), address.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+            "id=" + getId() +
+            ", email='" + getEmail() + "'" +
+            ", street='" + getStreet() + "'" +
+            ", city='" + getCity() + "'" +
+            ", postalCode='" + getPostalCode() + "'" +
+            ", country='" + getCountry() + "'" +
+            "}";
+    }
+}
---
+++ src/test/java/com/mycompany/myapp/web/rest/AddressResourceIntTest.java
@@ -0,0 +1,327 @@
+package com.mycompany.myapp.web.rest;
+
+import com.mycompany.myapp.MyappApp;
+
+import com.mycompany.myapp.domain.Address;
+import com.mycompany.myapp.repository.AddressRepository;
+import com.mycompany.myapp.repository.search.AddressSearchRepository;
+import com.mycompany.myapp.web.rest.errors.ExceptionTranslator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Test class for the AddressResource REST controller.
+ *
+ * @see AddressResource
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MyappApp.class)
+public class AddressResourceIntTest {
+
+    private static final String DEFAULT_EMAIL = "AAAAAAAAAA";
+    private static final String UPDATED_EMAIL = "BBBBBBBBBB";
+
+    private static final String DEFAULT_STREET = "AAAAAAAAAA";
+    private static final String UPDATED_STREET = "BBBBBBBBBB";
+
+    private static final String DEFAULT_CITY = "AAAAAAAAAA";
+    private static final String UPDATED_CITY = "BBBBBBBBBB";
+
+    private static final String DEFAULT_POSTAL_CODE = "AAAAAAAAAA";
+    private static final String UPDATED_POSTAL_CODE = "BBBBBBBBBB";
+
+    private static final String DEFAULT_COUNTRY = "AAAAAAAAAA";
+    private static final String UPDATED_COUNTRY = "BBBBBBBBBB";
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private AddressSearchRepository addressSearchRepository;
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Autowired
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    @Autowired
+    private ExceptionTranslator exceptionTranslator;
+
+    @Autowired
+    private EntityManager em;
+
+    private MockMvc restAddressMockMvc;
+
+    private Address address;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        AddressResource addressResource = new AddressResource(addressRepository, addressSearchRepository);
+        this.restAddressMockMvc = MockMvcBuilders.standaloneSetup(addressResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setControllerAdvice(exceptionTranslator)
+            .setMessageConverters(jacksonMessageConverter).build();
+    }
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Address createEntity(EntityManager em) {
+        Address address = new Address()
+            .email(DEFAULT_EMAIL)
+            .street(DEFAULT_STREET)
+            .city(DEFAULT_CITY)
+            .postalCode(DEFAULT_POSTAL_CODE)
+            .country(DEFAULT_COUNTRY);
+        return address;
+    }
+
+    @Before
+    public void initTest() {
+        addressSearchRepository.deleteAll();
+        address = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    public void createAddress() throws Exception {
+        int databaseSizeBeforeCreate = addressRepository.findAll().size();
+
+        // Create the Address
+        restAddressMockMvc.perform(post("/api/addresses")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(address)))
+            .andExpect(status().isCreated());
+
+        // Validate the Address in the database
+        List<Address> addressList = addressRepository.findAll();
+        assertThat(addressList).hasSize(databaseSizeBeforeCreate + 1);
+        Address testAddress = addressList.get(addressList.size() - 1);
+        assertThat(testAddress.getEmail()).isEqualTo(DEFAULT_EMAIL);
+        assertThat(testAddress.getStreet()).isEqualTo(DEFAULT_STREET);
+        assertThat(testAddress.getCity()).isEqualTo(DEFAULT_CITY);
+        assertThat(testAddress.getPostalCode()).isEqualTo(DEFAULT_POSTAL_CODE);
+        assertThat(testAddress.getCountry()).isEqualTo(DEFAULT_COUNTRY);
+
+        // Validate the Address in Elasticsearch
+        Address addressEs = addressSearchRepository.findOne(testAddress.getId());
+        assertThat(addressEs).isEqualToComparingFieldByField(testAddress);
+    }
+
+    @Test
+    @Transactional
+    public void createAddressWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = addressRepository.findAll().size();
+
+        // Create the Address with an existing ID
+        address.setId(1L);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restAddressMockMvc.perform(post("/api/addresses")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(address)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Alice in the database
+        List<Address> addressList = addressRepository.findAll();
+        assertThat(addressList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    @Transactional
+    public void checkEmailIsRequired() throws Exception {
+        int databaseSizeBeforeTest = addressRepository.findAll().size();
+        // set the field null
+        address.setEmail(null);
+
+        // Create the Address, which fails.
+
+        restAddressMockMvc.perform(post("/api/addresses")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(address)))
+            .andExpect(status().isBadRequest());
+
+        List<Address> addressList = addressRepository.findAll();
+        assertThat(addressList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void getAllAddresses() throws Exception {
+        // Initialize the database
+        addressRepository.saveAndFlush(address);
+
+        // Get all the addressList
+        restAddressMockMvc.perform(get("/api/addresses?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(address.getId().intValue())))
+            .andExpect(jsonPath("$.[*].email").value(hasItem(DEFAULT_EMAIL.toString())))
+            .andExpect(jsonPath("$.[*].street").value(hasItem(DEFAULT_STREET.toString())))
+            .andExpect(jsonPath("$.[*].city").value(hasItem(DEFAULT_CITY.toString())))
+            .andExpect(jsonPath("$.[*].postalCode").value(hasItem(DEFAULT_POSTAL_CODE.toString())))
+            .andExpect(jsonPath("$.[*].country").value(hasItem(DEFAULT_COUNTRY.toString())));
+    }
+
+    @Test
+    @Transactional
+    public void getAddress() throws Exception {
+        // Initialize the database
+        addressRepository.saveAndFlush(address);
+
+        // Get the address
+        restAddressMockMvc.perform(get("/api/addresses/{id}", address.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.id").value(address.getId().intValue()))
+            .andExpect(jsonPath("$.email").value(DEFAULT_EMAIL.toString()))
+            .andExpect(jsonPath("$.street").value(DEFAULT_STREET.toString()))
+            .andExpect(jsonPath("$.city").value(DEFAULT_CITY.toString()))
+            .andExpect(jsonPath("$.postalCode").value(DEFAULT_POSTAL_CODE.toString()))
+            .andExpect(jsonPath("$.country").value(DEFAULT_COUNTRY.toString()));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingAddress() throws Exception {
+        // Get the address
+        restAddressMockMvc.perform(get("/api/addresses/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updateAddress() throws Exception {
+        // Initialize the database
+        addressRepository.saveAndFlush(address);
+        addressSearchRepository.save(address);
+        int databaseSizeBeforeUpdate = addressRepository.findAll().size();
+
+        // Update the address
+        Address updatedAddress = addressRepository.findOne(address.getId());
+        updatedAddress
+            .email(UPDATED_EMAIL)
+            .street(UPDATED_STREET)
+            .city(UPDATED_CITY)
+            .postalCode(UPDATED_POSTAL_CODE)
+            .country(UPDATED_COUNTRY);
+
+        restAddressMockMvc.perform(put("/api/addresses")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(updatedAddress)))
+            .andExpect(status().isOk());
+
+        // Validate the Address in the database
+        List<Address> addressList = addressRepository.findAll();
+        assertThat(addressList).hasSize(databaseSizeBeforeUpdate);
+        Address testAddress = addressList.get(addressList.size() - 1);
+        assertThat(testAddress.getEmail()).isEqualTo(UPDATED_EMAIL);
+        assertThat(testAddress.getStreet()).isEqualTo(UPDATED_STREET);
+        assertThat(testAddress.getCity()).isEqualTo(UPDATED_CITY);
+        assertThat(testAddress.getPostalCode()).isEqualTo(UPDATED_POSTAL_CODE);
+        assertThat(testAddress.getCountry()).isEqualTo(UPDATED_COUNTRY);
+
+        // Validate the Address in Elasticsearch
+        Address addressEs = addressSearchRepository.findOne(testAddress.getId());
+        assertThat(addressEs).isEqualToComparingFieldByField(testAddress);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistingAddress() throws Exception {
+        int databaseSizeBeforeUpdate = addressRepository.findAll().size();
+
+        // Create the Address
+
+        // If the entity doesn't have an ID, it will be created instead of just being updated
+        restAddressMockMvc.perform(put("/api/addresses")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(address)))
+            .andExpect(status().isCreated());
+
+        // Validate the Address in the database
+        List<Address> addressList = addressRepository.findAll();
+        assertThat(addressList).hasSize(databaseSizeBeforeUpdate + 1);
+    }
+
+    @Test
+    @Transactional
+    public void deleteAddress() throws Exception {
+        // Initialize the database
+        addressRepository.saveAndFlush(address);
+        addressSearchRepository.save(address);
+        int databaseSizeBeforeDelete = addressRepository.findAll().size();
+
+        // Get the address
+        restAddressMockMvc.perform(delete("/api/addresses/{id}", address.getId())
+            .accept(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
+
+        // Validate Elasticsearch is empty
+        boolean addressExistsInEs = addressSearchRepository.exists(address.getId());
+        assertThat(addressExistsInEs).isFalse();
+
+        // Validate the database is empty
+        List<Address> addressList = addressRepository.findAll();
+        assertThat(addressList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+
+    @Test
+    @Transactional
+    public void searchAddress() throws Exception {
+        // Initialize the database
+        addressRepository.saveAndFlush(address);
+        addressSearchRepository.save(address);
+
+        // Search the address
+        restAddressMockMvc.perform(get("/api/_search/addresses?query=id:" + address.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(address.getId().intValue())))
+            .andExpect(jsonPath("$.[*].email").value(hasItem(DEFAULT_EMAIL.toString())))
+            .andExpect(jsonPath("$.[*].street").value(hasItem(DEFAULT_STREET.toString())))
+            .andExpect(jsonPath("$.[*].city").value(hasItem(DEFAULT_CITY.toString())))
+            .andExpect(jsonPath("$.[*].postalCode").value(hasItem(DEFAULT_POSTAL_CODE.toString())))
+            .andExpect(jsonPath("$.[*].country").value(hasItem(DEFAULT_COUNTRY.toString())));
+    }
+
+    @Test
+    @Transactional
+    public void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(Address.class);
+        Address address1 = new Address();
+        address1.setId(1L);
+        Address address2 = new Address();
+        address2.setId(address1.getId());
+        assertThat(address1).isEqualTo(address2);
+        address2.setId(2L);
+        assertThat(address1).isNotEqualTo(address2);
+        address1.setId(null);
+        assertThat(address1).isNotEqualTo(address2);
+    }
+}
